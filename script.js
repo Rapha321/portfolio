2dfa395364defb2a2556234e5dce554f091b5d5a
Rapha321 <ralpha19@hotmail.com>
2020-06-09 20:06:47 +0000

    added background

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(document).ready(function() {  // to make sure that the DOM of the page is read
 
 
   // When the user scrolls down 200px from the top of the document, show the button
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 200){
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 200) {
       $('.arrow-up').fadeIn();
     } else{
       $('.arrow-up').fadeOut();
@@ -13,11 +13,9 @@ $(document).ready(function() {  // to make sure that the DOM of the page is read
   });
 
   // when top arrow button is clicked, scrolled to the top
-  $(".arrow-up").click(function(){
+  $(".arrow-up").click(function() {
     $('html ,body').animate({scrollTop : 0},800);
   });
 
-  // $("#resume").click(function() {
-  //   $('html, main').innerHTML = url("")
-  // })
+
 })
