b8507f26c6c09c35f3be847191d63f84a1e0489d
Raphael Albern <ralpha19@hotmail.com>
2021-11-05 12:07:42 -0400

    update about me section

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-
-
-
-$(document).ready(function() {  // to make sure that the DOM of the page is ready to manipulate.
-
+$(document).ready(function () {
+  // to make sure that the DOM of the page is ready to manipulate.
 
   // When the user scrolls down 200px from the top of the document, show the button
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 200) {
-      $('.arrow-up').fadeIn();
-    } else{
-      $('.arrow-up').fadeOut();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".arrow-up").fadeIn();
+    } else {
+      $(".arrow-up").fadeOut();
     }
   });
 
   // when top arrow button is clicked, scrolled to the top
-  $(".arrow-up").click(function() {
-    $('html ,body').animate({scrollTop : 0},800);
+  $(".arrow-up").click(function () {
+    $("html ,body").animate({ scrollTop: 0 }, 800);
   });
-
-
-
-})
+});
